683861bc88a0a7cc275240e037c998d54cb59073
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-04 18:56:18 +0100

    Created common file with function to accept user input.

---
+++ src/core/common.py
@@ -0,0 +1,19 @@
+
+
+def getUserInput(prompt, options = None, returnType = str, inputFunction = raw_input):
+	"""
+	Prompts the user for input and returns the result.
+
+	:param prompt: The prompt to display.
+	:param options: Optional options the user must choose from, defaults to none.
+	:param returnType: Option return type, defaults to string.
+	:param inputFunction: The function to use to receive the user input, defaults to raw_input.
+	:return: The result casted to the given return type.
+	"""
+	optionString = " (%s)" % ", ".join(options) if options is not None else ""
+	userInput = inputFunction("%s%s" % (prompt, optionString))
+	if options is not None:
+		if userInput not in options:
+			print "\nPlease choose one from the following options:\n\n%s\n" % "\n".join(options)
+			userInput = getUserInput(prompt, options)
+	return returnType(userInput)
---
+++ src/tests/test_common.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2.7
+
+import unittest
+from ..core.common import *
+
+class TestCommon(unittest.TestCase):
+	def testCommon(self):
+		self.fail()
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
