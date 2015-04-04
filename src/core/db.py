8bc50013b6129bbd0e06738fafa55c7c8639e115
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-04 18:56:30 +0100

    Created rudimentary db class.

---
+++ src/core/db.py
@@ -0,0 +1,23 @@
+import MySQLdb
+
+class DB(object):
+	def __init__(self):
+		self._connect()
+
+	def _connect(self):
+		"""
+		Connects to the MySQL database and creates a cursor.
+
+		TODO: Should probably be using this within a with() to automatically
+		close the cursor after use. Should also use connection pooling.
+		"""
+		self.db = MySQLdb.connect(
+			host="localhost",
+			user="fake",
+			passwd="fake",
+			db="questionnaire"
+		)
+		self.dbHandle = self.db.cursor()
+
+	def query(self):
+		pass
