9a22067b435508d9ee085b7134bf76682060b051
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 00:37:56 +0100

    Updated db class with proper query methods and docstrings.

--- src/core/db.py
+++ src/core/db.py
@@ -1,6 +1,10 @@
 import MySQLdb
 
 class DB(object):
+	"""
+	Object encapsulating the database connection and operations.
+	"""
+
 	def __init__(self):
 		self._connect()
 
@@ -11,19 +15,41 @@ class DB(object):
 		TODO: Should probably be using this within a with() to automatically
 		close the cursor after use. Should also use connection pooling.
 		"""
-		self.db = MySQLdb.connect(
-			host="localhost",
-			user="fake",
-			passwd="fake",
-			db="questionnaire"
+		self._db = MySQLdb.connect(
+			host = "localhost",
+			user = "root",
+			passwd = "",
+			db = "questionnaire"
 		)
-		self.dbHandle = self.db.cursor()
+		self._dbHandle = self.db.cursor()
 
-	def query(self):
-		pass
+	def query(self, query):
+		"""
+		Executes the given query and returns the results.
+
+		:param query: The SQL to execute.
+		:return: :ist of column:value dictionaries.
+		"""
+		self._dbHandle.execute(query)
+		return self._dictFetchAll()
+
+	def _dictFetchAll(self):
+		"""
+		Creates a dictionary of column:value from the results of a raw query.
+
+		:return: Dictionary column:value.
+		"""
+		desc = self._dbHandle.description
+		return [
+			dict(zip([col[0] for col in desc], row))
+			for row in self._dbHandle.fetchall()
+		]
 
 	def close(self):
-		self.db.close()
-		self.dbHandle.close()
+		"""
+		Closes the handle and connection.
+		"""
+		self._db.close()
+		self._dbHandle.close()
 
 db = DB()
