04786b25bd4a02249079c1f3bd87094b742062e7
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 23:29:45 +0100

    Added getEligibleCount method to patient service.

--- src/core/services/patient_service.py
+++ src/core/services/patient_service.py
@@ -6,10 +6,21 @@ class PatientService(Service):
 		super(PatientService, self).__init__()
 
 	def get(self, id):
-		patientResult = self.db.query("SELECT * FROM patients WHERE id = %i", id)
+		"""
+		Gets a patient object from the database by the given id.
+
+		:param id: The id of the patient object to get.
+		:return:
+		"""
+		patientResult = self.db.query("SELECT * FROM patients WHERE id = %s", id)[0]
 		return Patient(patientResult["name"], int(patientResult["id"]), patientResult["joined"])
 
 	def getAll(self):
+		"""
+		Gets all patient objects from the database.
+
+		:return: List of patient objects.
+		"""
 		patientResults = self.db.query("SELECT * FROM patients")
 		return [Patient(patientResult["name"], int(patientResult["id"]), patientResult["joined"]) for patientResult in patientResults]
 
@@ -20,10 +31,29 @@ class PatientService(Service):
 		:param patient: Patient object to insert.
 		:return: Newly inserted patient object.
 		"""
-		self.db.query("INERT INTO patients (id, name, joined) VALUES ('', %s, '')", patient.name)
+		if patient.id is not None:
+			raise ValueError("Tried to create an already existent patient (%s)." % patient)
+
+		self.db.query("INSERT INTO patients (name) VALUES (%s)", patient.name)
 		self.db.commit()
 
 		# Reload the object to ensure auto rows are filled
 		return self.get(self.db.lastRowId())
+
+	def getEligibleCount(self):
+		"""
+		Gets the total number of currently eligible patients (as of the last time they took the questionnaire).
+
+		:return: The number of eligible patients.
+		"""
+		eligibleCount = self.db.query(
+			"SELECT COUNT(sessions.eligible) as count "
+			"FROM patients "
+			"LEFT JOIN sessions "
+			"ON sessions.patient_id = patients.id "
+			"GROUP BY patients.id "
+			"ORDER BY sessions.created DESC"
+		)
+		return int(eligibleCount["count"])
 
 patientService = PatientService()
