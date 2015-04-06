0c0fc9da7f781fce73400e3d8906806c78e6ef9d
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 23:28:22 +0100

    Created questionnaire test skeleton.

---
+++ src/tests/test_questionnaire.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python2.7
+
+import unittest
+from ..core.questionnaire import *
+
+class TestQuestionnaire(unittest.TestCase):
+	pass
+
+def main():
+	unittest.main()
+
+if __name__ == '__main__':
+	main()
