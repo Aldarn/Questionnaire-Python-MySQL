681ea8a76be02a59b1a06c4788722ed4cc8fc3cd
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 20:56:38 +0100

    Added eligible field to session objects to easily determine if a session is eligible or not.

--- src/core/domain/session.py
+++ src/core/domain/session.py
@@ -1,10 +1,11 @@
 from db_object import DBObject
 
 class Session(DBObject):
-	def __init__(self, patient_id, id = None, created = None):
+	def __init__(self, patient_id, id = None, created = None, eligible = False):
 		self._patientId = patient_id
 		self._id = id
 		self._created = created
+		self._eligible = eligible
 
 	@property
 	def id(self):
@@ -18,8 +19,16 @@ class Session(DBObject):
 	def created(self):
 		return self._created
 
+	@property
+	def eligible(self):
+		return self._eligible
+
+	@eligible.setter
+	def eligible(self, eligible):
+		self._eligible = eligible
+
 	def __str__(self):
 		return "%i %s" % (self.patientId, self.created)
 
 	def __repr__(self):
-		return "Session(%i, %i, %s)" % (self.id, self.patientId, self.created)
+		return "Session(%i, %i, %s, %r)" % (self.id, self.patientId, self.created, self.eligible)
