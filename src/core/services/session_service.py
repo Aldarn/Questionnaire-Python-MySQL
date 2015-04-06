cb800b14d7b67a65e94dac481f694ef5e05041c9
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 21:01:45 +0100

    Added method to update the eligibility of a session.

--- src/core/services/session_service.py
+++ src/core/services/session_service.py
@@ -7,4 +7,19 @@ class SessionService(Service):
 	def create(self, obj):
 		pass
 
+	def updateEligibility(self, session, eligible):
+		"""
+		Updates the eligibility of an existing session.
+
+		:param session: The session to update.
+		:param eligible: If the session is eligible or not.
+		:return: Updated session object.
+		"""
+		if session.id is None:
+			raise ValueError("Tried to update the eligibility of a session that has not been saved in the database.")
+
+		session.eligible = eligible
+		self.db.query("UPDATE sessions SET eligible = %i WHERE id = %i", (1 if eligible else 0, session.id))
+		return session
+
 sessionService = SessionService()
