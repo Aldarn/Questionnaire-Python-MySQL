3b938a3ce4d313a9670c343fb95d166651a773da
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 15:07:51 +0100

    Answer service create method implemented.

--- src/core/services/answer_service.py
+++ src/core/services/answer_service.py
@@ -4,7 +4,14 @@ class AnswerService(Service):
 	def __init__(self):
 		super(AnswerService, self).__init__()
 
-	def create(self, obj):
-		pass
+	def create(self, answer):
+		"""
+		Inserts the given answer into the database.
+
+		:param answer: Answer object to insert.
+		"""
+		self.db.query("INERT INTO answers (question_id, session_id, answer) VALUES (%s, %s, %s)",
+			(answer.questionId, answer.sessionId, answer.answer))
+		self.db.commit()
 
 answerService = AnswerService()
