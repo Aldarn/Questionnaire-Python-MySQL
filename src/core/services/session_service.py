6e32ea4c1f46d90190f225fa2438232145552ff8
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-06 23:30:47 +0100

    Implemented session service get and create methods.

--- src/core/services/session_service.py
+++ src/core/services/session_service.py
@@ -1,11 +1,46 @@
 from service import Service
+from ..domain.session import Session
 
 class SessionService(Service):
 	def __init__(self):
 		super(SessionService, self).__init__()
 
-	def create(self, obj):
-		pass
+	def get(self, id):
+		"""
+		Gets a session object with the given id from the database.
+
+		:param id: The id of the session to get.
+		:return: The loaded session object.
+		"""
+		sessionResult = self.db.query("SELECT * FROM sessions WHERE id = %s", id)[0]
+		return self._map(sessionResult)
+
+	def getPatientSessions(self, patientId):
+		"""
+		Gets all sessions for the given patient id.
+
+		:param patientId: The patient id to get the sessions for.
+		:return: List of session objects.
+		"""
+		sessionResults = self.db.query("SELECT * FROM sessions WHERE patient_id = %s", patientId)
+		return [self._map(sessionResult) for sessionResult in sessionResults]
+
+	def create(self, session):
+		"""
+		Inserts the given session into the database.
+
+		:param session: Session object to insert.
+		:return: Newly inserted session object.
+		"""
+		if session.id is not None:
+			raise ValueError("Tried to create an already existent session (%s)." % session)
+
+		self.db.query("INSERT INTO sessions (patient_id, eligible) VALUES (%s, %s)",
+			session.patientId, 1 if session.eligible else 0)
+		self.db.commit()
+
+		# Reload the object to ensure auto rows are filled
+		return self.get(self.db.lastRowId())
 
 	def updateEligibility(self, session, eligible):
 		"""
@@ -16,10 +51,20 @@ class SessionService(Service):
 		:return: Updated session object.
 		"""
 		if session.id is None:
-			raise ValueError("Tried to update the eligibility of a session that has not been saved in the database.")
+			raise ValueError("Tried to update the eligibility of a session that has not been saved in the database (%s)." % session)
 
 		session.eligible = eligible
-		self.db.query("UPDATE sessions SET eligible = %i WHERE id = %i", (1 if eligible else 0, session.id))
+		self.db.query("UPDATE sessions SET eligible = %s WHERE id = %s", 1 if eligible else 0, session.id)
 		return session
+
+	def _map(self, sessionResult):
+		"""
+		Maps a session database result to a session object.
+
+		:param sessionResult: Session database result.
+		:return: Session object.
+		"""
+		return Session(int(sessionResult["patient_id"]), int(sessionResult["id"]), sessionResult["created"],
+			bool(sessionResult["eligible"]))
 
 sessionService = SessionService()
