05038594a752e547163b901aadc5ee4a2f9cf1bd
Benjamin Holmes <benjamin.holmes@visualdna.com>
2015-04-09 20:02:35 +0100

    Created setup.py.

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name             = 'questionnaire',
+    version          = '1.0.0',
+    author           = 'Benjamin David Holmes',
+    author_email     = 'ben@bdholmes.com',
+    url              = 'https://bitbucket.org/Aldarn/questionnaire',
+    download_url     = 'https://bitbucket.org/Aldarn/questionnaire/downloads',
+    description      = 'Simple MySQL driven command line questionnaire with per-question percentage chance of acceptance.',
+    py_modules       = ['Questionnaire'],
+    package_dir      = {'': 'src'},
+    license          = 'GPLv3+',
+    long_description = open( "readme.md" ).read(),
+	install_requires = ['MySQL-python', 'fake-factory'],
+	test_requires 	 = ['mock'],
+    classifiers 	 = [
+        'Environment :: Console',
+		'Intended Audience :: End Users/Desktop',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
+        'Topic :: Other/Nonlisted Topic',
+		'Topic :: Scientific/Engineering :: Information Analysis'
+    ]
+) 
